6d08abb0e366de6c88ab077f434f419ba6342122
Student6 <Student6@10.8.33.175>
2016-09-22 12:33:59 -0400

    9.22 practice

--- Introduction/src/datatypes/StringPractice.java
+++ Introduction/src/datatypes/StringPractice.java
@@ -1,26 +1,74 @@
 package datatypes;
 
-public class StringPractice {
+import java.util.Scanner;
 
+public class StringPractice {
+	
+	static Scanner input;
+	static String user;
+	static int lineCount;
+	
 	public static void main(String[] args){
-		//String text= new String("Hello World";)
-		String text1= "Hello World";
-		String text2= "Hello ";
-		String text3= "World";
+		createAScanner();
+		lineCount=0;
+		demonstrateStringMethods();
+		promptName();
+		talkForever();
 		
-		if (text1.equals( text2+text3)){ //not (text1 == text3) 
-		System.out.println("These strings are equal.");	
+	}
+	
+	public static void promptName(){
+		print("Hello, human! I am a "
+				+"board covered with "
+				+"semiconductors and \n"
+				+"other such electronic components. "
+				+"What is your name?");
+				user=input.nextLine();
+				print("Awesome! I will call you "+user+ " until you terminate me.");
+	}
+	
+	public static void talkForever(){
+		while(true){
+			promptInput();
 		}
-		
-		System.out.println(text1);
-		System.out.println(text2 + text3);
-		
-		String word1= "Aardvark";
-		String word2="ZZZ";
-		if (word1.compareTo(word2)<0){
-		System.out.println("Word1 is before word2," + " lexicongraphically.");	
+	}
+
+	public static void promptInput(){
+		print("Try inputing a String!");
+		String userInput = input.nextLine();
+		print(userInput);
+	}
+	
+	public static void print(String s){
+		lineCount++;
+		System.out.println("Line #"+lineCount+":" +s);
+	}
+	
+	public static void createAScanner() {
+		input=new Scanner(System.in);
+	}
+	
+	public static void demonstrateStringMethods(){
+			//String text= new String("Hello World";)
+			String text1= "Hello World";
+			String text2= "Hello ";
+			String text3= "World";
+			
+			if (text1.equals( text2+text3)){ //not (text1 == text3) 
+			System.out.println("These strings are equal.");	
+			}
+			
+			System.out.println(text1);
+			System.out.println(text2 + text3);
+			
+			String word1= "Aardvark";
+			String word2="ZZZ";
+			if (word1.compareTo(word2)<0){
+			System.out.println("Word1 is before word2," + " lexicongraphically.");	
+				
+			}
 			
 		}
 		
 	}
-}
+
