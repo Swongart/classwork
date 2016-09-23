9592669e93c96666bcd240e2590a0318a2600cbb
Student6 <Student6@10.8.33.175>
2016-09-23 12:27:47 -0400

    string practice_9/23/16

--- Introduction/src/datatypes/StringPractice.java
+++ Introduction/src/datatypes/StringPractice.java
@@ -40,8 +40,34 @@ public class StringPractice {
 	}
 	
 	public static void print(String s){
-		lineCount++;
-		System.out.println("Line #"+lineCount+":" +s);
+		//lineCount++;
+		//create a multi-line String
+		String printString = "";
+		int cutoff= 35;
+		//check to see if there are words to add
+		//(IOW is the length of s>0
+		while(s.length()>0){
+			String currentLine="";
+			String nextWord="";
+			//while the currentLine and nextWord are less than the cuttoff,
+			// and there are still words to add, do the following loop.
+		while(currentLine.length()+
+				nextWord.length() <= cutoff && s.length()>0){
+			//add the next word to the line
+			currentLine+=nextWord;
+			//then, remove that word
+			s=s.substring(nextWord.length());
+			//get the following word.
+			int endOfWord=s.indexOf(" ");
+			//check to see if this is the last word
+			if (endOfWord==-1){
+				endOfWord= s.length()-1;
+			}
+			nextWord=s.substring(0,endOfWord+1);
+		}
+		printString+= currentLine+"\n";
+		}
+		System.out.println(printString);
 	}
 	
 	public static void createAScanner() {
