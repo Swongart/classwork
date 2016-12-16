2b1e19c7670786c0c564fec98c70b35d83f78471
Period 6 2.0 <Period 6 2.0@10.8.33.175>
2016-12-16 13:18:51 -0500

    there is an err

--- Introduction/src/gui/Screens/CoordinateScreen.java
+++ Introduction/src/gui/Screens/CoordinateScreen.java
@@ -1,22 +1,56 @@
 package gui.Screens;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import gui.Screen;
+import gui.Components.Action;
+import gui.Components.Button;
 import gui.Components.TextLabel;
 import gui.Components.Visible;
 
-public class CoordinateScreen extends Screen {
 
+public class CoordinateScreen extends Screen implements MouseMotionListener{
+
+	public Button button;
+	private TextLabel text;
+	
 	public CoordinateScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
+		button=new Button(20,100,80,40,"Button", new Color(100,100,250), new Action()){
+			public void act(){
+				
+			}
+		};
+			
+		
 		TextLabel text = new TextLabel(20, 200, 500, 40, "Some text");
 		viewObjects.add(text);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		int mx= e.getX(); // get mouse X coord
+		int my= e.getY(); // get mouse Y coord
+		text.setText("Mouse at: "+mx+", "+my);
+		
+		
+	}
+	public MouseMotionListener getMouseMotionListener(){
+		return this;
 	}
 
 }
